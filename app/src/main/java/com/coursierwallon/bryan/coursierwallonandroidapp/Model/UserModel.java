c64f510a99b6bd894c0bcd2163aca8cf835a554c
Bryan <francotte.bryan@gmail.com>
2017-12-21 19:04:19 +0100

    added regex email

--- app/src/main/java/com/coursierwallon/bryan/coursierwallonandroidapp/Model/UserModel.java
+++ app/src/main/java/com/coursierwallon/bryan/coursierwallonandroidapp/Model/UserModel.java
@@ -1,6 +1,9 @@
 package com.coursierwallon.bryan.coursierwallonandroidapp.Model;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by franc on 11-11-17.
  */
@@ -24,9 +27,13 @@ public class UserModel {
     }
 
     public boolean isvalid(){
+        Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+
         boolean validExpression =
                 !userName.isEmpty() && !email.isEmpty() && !password.isEmpty()
-                && !passwordConfirmation.isEmpty() && password.compareTo(passwordConfirmation) == 0;
+                && !passwordConfirmation.isEmpty() && password.compareTo(passwordConfirmation) == 0
+                && matcher.find();
         return validExpression;
     }
 }
